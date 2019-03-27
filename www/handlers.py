e583a96b729a9e9a67dc901d0bfe684b4e3412b0
冰白 <724900477@qq.com>
2019-03-27 18:49:45 +0800

    cookie setting's first step

--- www/handlers.py
+++ www/handlers.py
@@ -1,17 +1,55 @@
 import base64
+import hashlib
 import json
 import os
 import re
 import time
 import uuid
+
+from aiohttp import web
+import logging;logging.basicConfig(level=logging.INFO)
+
 from _sha256 import sha256
 from hmac import HMAC
-from aiohttp import web
 from www.apis import APIValueError, APIError
+from www.config import configs
 from www.coroweb import get, post
 from www.models import User, Comment, Blog, next_id
 
 _RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
+COOKIE_NAME = 'iwsession'
+_COOKIE_KEY = configs.session.secret
+
+
+def user2cookie(user, max_age):
+    expires = str(int(time.time() + max_age))
+    s = '%s-%s-%s-%s' % (user.id, user.passwd, expires, _COOKIE_KEY)
+    L = [user.id, expires, hashlib.sha1(s.encode('utf-8')).hexdigest()]
+    return '-'.join(L)
+
+
+async def cookie2user(cookie_str):
+    if not cookie_str:
+        return None
+    try:
+        L = cookie_str.split('-')
+        if len(L) != 3:
+            return None
+        uid, expires, sha1 = L
+        if int(expires) < time.time():
+            return None
+        user = await User.find(uid)
+        if user is None:
+            return None
+        s = '%s-%s-%s-%s' % (uid, user.passwd, expires, _COOKIE_KEY)
+        if sha1 != hashlib.sha1(s.encode('utf-8')).hexdigest():
+            logging.info('invalid sha1')
+            return None
+        user.passwd = '******'
+        return user
+    except Exception as e:
+        logging.exception(e)
+        return None
 
 
 def next_imgid():
@@ -65,7 +103,7 @@ async def index(request):
 @get('/article/{id}')
 async def api_article_getter(*, id):
     blog = await Blog.find(id)
-    comments = await Comment.findAll(['blog_id','show'], [id, True])
+    comments = await Comment.findAll(['blog_id', 'show'], [id, True])
     news = await Blog.findAll(orderBy='`created_at` DESC', limit=3)
     return {
         '__template__': '__article__.html',
@@ -94,6 +132,39 @@ async def api_contact_getter(request):
     }
 
 
+@post('/authenticate')
+async def api_user_authenticate(*,email, passwd):
+    if not email:
+        raise APIValueError('email', 'Invalid email.')
+    if not passwd:
+        raise APIValueError('passwd', 'Invalid password.')
+    users = await User.findAll('email', [email])
+    if len(users) == 0:
+        raise APIValueError('email', 'Email not exist.')
+    user = users[0]
+    sha1 = hashlib.sha1()
+    sha1.update(user.id.encode('utf-8'))
+    sha1.update(b':')
+    sha1.update(passwd.encode('utf-8'))
+    if user.passwd != sha1.hexdigest():
+        raise APIValueError('passwd', 'Invalid password.')
+    r = web.Response()
+    r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
+    user.passwd = '******'
+    r.content_type = 'application/json'
+    r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
+    return r
+
+
+@get('/signout')
+def api_user_signout(request):
+    referer = request.headers.get('Referer')
+    r = web.HTTPFound(referer or '/')
+    r.set_cookie(COOKIE_NAME, '-deleted-', max_age=0, httponly=True)
+    logging.info('user signed out.')
+    return r
+
+
 @post('/users')
 async def api_register_user(*, email, name, passwd):
     if not name or not name.strip():
@@ -120,12 +191,12 @@ async def api_add_blog(*, name, intro, body, img):
     with open('./static/images/blogintro/' + uid + '.png', 'wb') as store:
         store.write(base64.b64decode(img[22:].encode()))
     # body = base64.b64decode(body).decode() #取
-    blog = Blog(id=uid, user_id='null', user_image='138649796585137407.jpg',user_name='冰白', name=name, summary=intro, content=body, img=uid+'.png')
+    blog = Blog(id=uid, user_id='null', user_image='/userimg/138649796585137407.jpg',user_name='冰白', name=name, summary=intro, content=body, img='/blogintro/' + uid+'.png')
     await blog.save()
 
 
 @post('/imgurl')
-async def saveimg(**kw):
+async def api_save_img(**kw):
     imgid = next_imgid()
     with open('./static/images/blogimg/' + imgid + '.' + kw['upload'].content_type.replace('image/', ''), 'wb') as store:
         store.write(kw['upload'].file.read())
@@ -136,25 +207,6 @@ async def saveimg(**kw):
 async def staticGetter(*, dictionary, filename):
     with open('./static/images/'+ dictionary + '/' + filename, 'rb') as f:
         return f.read()
-
-
-@get('/userimg/{filename}')
-async def user_img_getter(filename):
-    with open('./static/images/userimg/' + filename, 'rb') as f:
-        return f.read()
-
-
-@get('/test')
-async def test(request):
-    blogs = await Blog.findAll()
-    blog = blogs[0]
-    comments = await Comment.findAll(where=['blog_id', 'show'], args=[blog.id, True])
-    # blogs[0].created_at = datetime_filter(blogs[0].created_at)
-    return{
-        '__template__': 'page.html',
-        'blog': blog,
-        'comments': comments
-    }
 
 
 @post('/comments')
